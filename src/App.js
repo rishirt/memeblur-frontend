14c23a286bbabeeb35919780ef616b09c6a4d2cb
Rishi Thulasiraman <rishithulasiramanr@gmail.com>
2019-01-07 21:57:48 +0530

    Added Logo and Navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        {/* <Logo />
+        <Logo />
         <ImageLinkForm />
+        {/* 
         <FaceRecognition /> */}
       </div>
     );
