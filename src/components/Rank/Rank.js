55aafe9ef2d813f1549a7875b931f5f0d369ad38
Rishi Thulasiraman <rishithulasiramanr@gmail.com>
2019-01-07 23:21:41 +0530

    added particle.js

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div className = 'white'>
+      <div className='f3'>
+        {'Rishi, Your Current Rank is...'}
+      </div>
+      <div className='f1'>
+        {'#3'}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
