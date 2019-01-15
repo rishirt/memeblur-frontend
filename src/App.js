a30a3c6abd2b4b9dd9b0de564ca949bd7a3d7307
Rishi Thulasiraman <rishithulasiramanr@gmail.com>
2019-01-16 00:43:55 +0530

    update clarifai api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import SignIn from './components/SignIn/SignIn';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '4ab0bf448dbb4b45a11be7d373006990'
+});
 
 const particlesOptions = {
   particles: {
@@ -21,70 +25,80 @@ const particlesOptions = {
   }
 }
 
-const app = new Clarifai.App({
-  apiKey: '4ab0bf448dbb4b45a11be7d373006990'
- });
-
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      input : '',
-      imageUrl : '',
-      box : {}
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
     }
   }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col*width),
-      bottomRow: height - (clarifaiFace.bottom_row*height)
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   }
+
   displayFaceBox = (box) => {
-    this.setState({box:box});
+    this.setState({box: box});
   }
-  
+
   onInputChange = (event) => {
-    this.setState({input : event.target.value});
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl : this.state.input})
+    this.setState({imageUrl: this.state.input});
     app.models
-    .predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-    .catch(err => console.log(err));
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
   }
 
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <Particles className='particles'
-          params={particlesOptions}
-        />
-        <div className='flex justify-between'>
-          <Logo />
-          <Navigation />
+        <Particles className='particles' params={particlesOptions} />
+        <div className = 'flex justify-between'>
+          <Logo/>
+          <Navigation isSignedIn = {isSignedIn} onRouteChange = {this.onRouteChange}/>
         </div>
-        <SignIn/>
-        <Rank/>
-        <ImageLinkForm 
-          onInputChange = {this.onInputChange}
-          onButtonSubmit = {this.onButtonSubmit}
-        />
-        <FaceRecognition 
-        imageUrl = {this.state.imageUrl}
-        box = {this.state.box}
-        />
+        {
+          route === 'home' 
+          ?<div>
+            <ImageLinkForm 
+            onInputChange={this.onInputChange}
+            onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+          :(route === 'signin')
+            ?<Signin onRouteChange={this.onRouteChange}/>
+            :<Register onRouteChange={this.onRouteChange}/>
+        }
       </div>
     );
   }
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,34 +1,63 @@
 import React from 'react';
 
-class Signin extends React.Component {
+class Register extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      signInEmail: '',
-      signInPassword: ''
+      email: '',
+      password: '',
+      name: ''
     }
   }
 
+  onNameChange = (event) => {
+    this.setState({name: event.target.value})
+  }
+
   onEmailChange = (event) => {
-    this.setState({signInEmail: event.target.value})
+    this.setState({email: event.target.value})
   }
 
   onPasswordChange = (event) => {
-    this.setState({signInPassword: event.target.value})
+    this.setState({password: event.target.value})
   }
 
   onSubmitSignIn = () => {
-    this.props.onRouteChange('home');
+    fetch('http://localhost:3000/register', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name
+      })
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user) {
+          this.props.loadUser(user)
+          this.props.onRouteChange('home');
+        }
+      })
   }
 
   render() {
-    const { onRouteChange } = this.props;
     return (
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={this.onNameChange}
+                />
+              </div>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input
@@ -55,11 +84,8 @@ class Signin extends React.Component {
                 onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
-                value="Sign in"
+                value="Register"
               />
-            </div>
-            <div className="lh-copy mt3">
-              <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
           </div>
         </main>
@@ -68,4 +94,4 @@ class Signin extends React.Component {
   }
 }
 
-export default Signin;
+export default Register;
