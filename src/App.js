9d7a1a2049cd6a83b2b753b748e9762c6ee12a43
Rishi Thulasiraman <rishithulasiramanr@gmail.com>
2019-01-29 23:55:04 +0530

    update bounding box styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank'
 import './App.css';
 
 const app = new Clarifai.App({
- apiKey: '4ab0bf448dbb4b45a11be7d373006990'
+ apiKey: 'd47bed7952604b74824e9dc607600f39'
 });
 
 const particlesOptions = {
@@ -31,76 +29,41 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
     }
-  }
-  
-  calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
-
-  displayFaceBox = (box) => {
-    this.setState({box: box});
   }
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({
+      input : event.target.value
+    })
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch(err => console.log(err));
-  }
-
-  onRouteChange = (route) => {
-    if (route === 'signout') {
-      this.setState({isSignedIn: false})
-    } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      // there was an error
     }
-    this.setState({route: route});
+  );
   }
+  
 
   render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className='particles' params={particlesOptions} />
         <div className = 'flex justify-between'>
           <Logo/>
-          <Navigation isSignedIn = {isSignedIn} onRouteChange = {this.onRouteChange}/>
+          <Navigation/>
         </div>
-        {
-          route === 'home' 
-          ?<div>
-            <Rank />
-            <ImageLinkForm 
-            onInputChange={this.onInputChange}
-            onButtonSubmit={this.onButtonSubmit}
-            />
-            <FaceRecognition box={box} imageUrl={imageUrl} />
-          </div>
-          :(route === 'signin')
-            ?<Signin onRouteChange={this.onRouteChange}/>
-            :<Register onRouteChange={this.onRouteChange}/>
-        }
+        <Rank />
+        <ImageLinkForm 
+        onInputChange = {this.onInputChange}
+        onButtonSubmit = {this.onButtonSubmit}
+        />
+        <FaceRecognition />
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl , box }) => {
+const FaceRecognition = () => {
   return (
-    <div className ='center ma'>
-      <div className = 'absolute mt2'>
-        <img src = {imageUrl}
-         id = 'inputImage'
-         alt = ''
-         width = '500px'
-         height = 'auto' />
-         <div 
-         className = 'bounding-box'
-         style = {{
-          top : box.topRow, 
-          right : box.rightCol, 
-          bottom : box.bottomRow, 
-          left : box.leftCol
-          }}>
-         </div>
-      </div>
+    <div>
     </div>
   );
 }
